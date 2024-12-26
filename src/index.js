d82d24245152d612efce762b08548c23b78a90a8
BAO TRANG <baogtrang@gmail.com>
2024-12-26 10:34:06 -0600

    NavLink

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from "react-router";
 import Admins from './components/Admins/Admins';
 import Users from './components/Users/Users';
+import HomePage from './components/Home/HomePage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -16,6 +17,7 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} >
+          <Route index element={<HomePage />} />
           <Route path="users" element={<Users />} />
           <Route path="admins" element={<Admins />} />
         </Route>
